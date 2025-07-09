77900735df3ac923e60527414b56f649e224b2d1
GaafarBBK <mohamedgaafar80@gmail.com>
2025-07-09 12:50:56 +0200

    Initiliaze project & migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('user_type', ['admin', 'user'])->default('user');
+            $table->enum('sex', ['male', 'female'])->nullable();
+            $table->date('birth_date')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
